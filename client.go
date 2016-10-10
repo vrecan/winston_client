e430c069c5710fd248f67cf53841a1a0722af47d
Ben Aldrich <ben.aldrich@logrhythm.com>
2016-10-10 01:44:02 +0000

    working client with new changes

--- client.go
+++ client.go
@@ -12,8 +12,11 @@ import (
 	"io"
 	"time"
 	// "net"
+	// "sync"
 	SYS "syscall"
 )
+
+const bulkCnt = 50
 
 func main() {
 	defer log.Flush()
@@ -56,150 +59,187 @@ func main() {
 	client := pb.NewV1Client(conn)
 	setupRepo(client)
 
-	go func() {
-		for {
-			// runPush(client)
-			runPullBuckets(client)
-			
-			time.Sleep(2 * time.Second)
-		}
-	}()
 	// go func() {
 	// 	for {
-	// 		runPush(client)
-	// 		time.Sleep(2 * time.Second)
+	// 		err := runPush(client)
+	// 		if err != nil {
+	// 			log.Error("Failed to push sleeping")
+	// 			time.Sleep(2 * time.Second)
+	// 		}
+	// 	}
+	// }()
+	// go func() {
+	// 	for {
+	// 		err := runPush(client)
+	// 		if err != nil {
+	// 			log.Error("Failed to push sleeping")
+	// 			time.Sleep(2 * time.Second)
+	// 		}
+
 	// 	}
 	// }()
 
 	// go func() {
 	// 	for {
-	// 		runPullBuckets(client)
-	// 		time.Sleep(2 * time.Second)
+	// 		err := runPush(client)
+	// 		if err != nil {
+	// 			log.Error("Failed to push sleeping")
+	// 			time.Sleep(2 * time.Second)
+	// 		}
 	// 	}
 	// }()
+
 	// go func() {
 	// 	for {
-	// 		runPullBuckets(client)
-	// 		time.Sleep(2 * time.Second)
+	// 		err := runPush(client)
+	// 		if err != nil {
+	// 			log.Error("Failed to push sleeping")
+	// 			time.Sleep(2 * time.Second)
+	// 		}
 	// 	}
 	// }()
+
+	go func() {
+		for {
+			runPullBuckets(client)
+			time.Sleep(2 * time.Second)
+
+			// 		err := runPush(client)
+			// 		if err != nil {
+			// 			log.Error("Failed to push sleeping")
+			// 			time.Sleep(2 * time.Second)
+			// 		}
+
+		}
+	}()
 
 	death.WaitForDeath()
 	log.Info("shutdown")
 }
 
 func getBuckets(client pb.V1Client) []string {
-	stream, err := client.GetBuckets(context.Background(), &pb.BucketsRequest{
-		Repo:        "name0",
-		StartTimeMs: uint64(time.Now().Add(-72*time.Hour).UnixNano() / int64(time.Millisecond)),
-		EndTimeMs:   uint64(time.Now().UnixNano() / int64(time.Millisecond)),
+	stream, err := client.GetPartitions(context.Background(), &pb.PartitionsRequest{
+		Repo:      "name0",
+		StartTime: time.Now().Add(-72 * time.Hour).Format(time.RFC3339),
+		EndTime:   time.Now().Format(time.RFC3339),
 	})
 	if err != nil {
 		log.Error("get buckets: ", err)
+		return []string{}
 	}
-	buckets := []string{}
+	partitions := []string{}
 	for {
 		in, err := stream.Recv()
 		if err == io.EOF {
 			log.Info("Finished getting buckets")
-			return buckets
+			return partitions
 		}
-		log.Info("BUCKET RECV: ", in)
-		buckets = append(buckets, in.Path)
+		log.Info("PARTITIONS: ", in)
+		partitions = append(partitions, in.Path)
 
 	}
-	return buckets
+	return partitions
 }
 
 func setupRepo(client pb.V1Client) {
 	_, err := client.UpsertRepo(context.Background(), &pb.RepoSettings{
-		Repo:           "name0",
-		Format:         pb.RepoSettings_JSON,
-		TimeField:      "normalDate",
-		GroupByFields:  []string{0: "id"},
-		GroupByBuckets: 8})
+		Repo:              "name0",
+		Format:            pb.RepoSettings_JSON,
+		TimeField:         "normalDate",
+		GroupByFields:     []string{0: "id"},
+		GroupByPartitions: 32})
 	if err != nil {
 		panic(fmt.Sprintf("Failed to setup repo: %v", err))
 	}
 }
 
 func runPullBuckets(client pb.V1Client) {
-	buckets := getBuckets(client)
-	
-	for _, b := range buckets {
-		t := time.Now()
-		func() {
-			pull := pb.ReadBucket{
-				Repo:        "name0",
-				StartTimeMs: uint64(t.Add(-24*time.Hour).UnixNano() / int64(time.Millisecond)),
-				EndTimeMs:   uint64(t.UnixNano() / int64(time.Millisecond)),
-				BucketPath:  b,
-			}
-			stream, err := client.ReadBucketByTime(context.Background(), &pull)
-			if err != nil {
-				log.Error("Failed to get stream from pullBucketByTime: ", err)
-				return
-			}
-			cnt := 0
-			rcds := 0
-			for {
-				resp, err := stream.Recv()
-				if err == io.EOF {
-					duration := time.Since(t)
-					log.Info("msgs rcvd: ", cnt, " ", duration, " bucket: ", b)
-					log.Info("rcds rcvd: ", rcds, " ", duration, " bucket: ", b)
-					return
-				}
-				if err != nil {
-					log.Error("Failed to read row from winston: ", err, " resp: ", resp)
-					return
-				}
-				rcds += len(resp.Rows)
-				cnt++
-			}
-		}()
+	t := time.Now()
+	it := time.Now()
+	pull := pb.Read{
+		Repo:      "name0",
+		StartTime: t.Add(-72 * time.Hour).Format(time.RFC3339),
+		EndTime:   t.Format(time.RFC3339),
+		BatchSize: 100,
 	}
+
+	stream, err := client.ReadByTime(context.Background(), &pull)
+	if err != nil {
+		log.Error("Failed to get stream from pullBucketByTime: ", err)
+		return
+	}
+	cnt := 0
+	rcds := 0
+	sampRcds := 0
+	for {
+		resp, err := stream.Recv()
+		if err == io.EOF {
+			duration := time.Since(t)
+			log.Info("msgs: ", cnt, " rcds: ", rcds, " took: ", duration)
+			return
+		}
+		if err != nil {
+			log.Error("Failed to read row from winston: ", err, " resp: ", resp)
+			return
+		}
+		rcds += len(resp.Rows)
+		sampRcds += len(resp.Rows)
+		if cnt%1000 == 0 {
+			if len(resp.Rows) > 20 {
+				// log.Info("0: ", resp.Rows[0])
+				// log.Info("10: ", resp.Rows[10])
+				duration := time.Since(it)
+				log.Info("read rate/s: ", float64(sampRcds)/duration.Seconds())
+				it = time.Now()
+				sampRcds = 0
+			}
+		}
+		cnt++
+	}
+	return
 
 }
 
-func runPush(client pb.V1Client) {
+func runPush(client pb.V1Client) error {
 	// stream, err := client.Push(context.Background())
 	// if err != nil {
 	// 	grpclog.Fatalf("%v.Push(_) = _, %v", client, err)
 	// }
+	data := MakeRequests(2000, bulkCnt)
 	now := time.Now()
 	cnt := 0
-	bulkCnt := 2000
-	for {
 
-		data := MakeRequests(50, bulkCnt)
+	for {
 
 		stream, err := client.Write(context.Background())
 		if err != nil {
 			log.Error("Failed to push stream to context background: ", err)
-			return
+			return err
 		}
 
 		for _, note := range data {
 			if err := stream.Send(note); err != nil {
-				log.Error("write stream send: ", err)
-				return
+				log.Error("write stream send failed: ", err)
+				return err
 			}
 			cnt++
 		}
 		_, err = stream.CloseAndRecv()
+		if err == io.EOF {
+			log.Error("Unexpected OF when trying to close the stream")
+			return err
+		}
 		if err != nil {
 			log.Error("Transaction failed for stream: ", err)
-			return
+			return err
 		}
+		break
 
-		if cnt*bulkCnt >= 100000 {
-			break
-		}
 	}
 
 	duration := time.Since(now)
-	log.Info(cnt*bulkCnt, " messages in ", duration)
+	log.Info("batches: ", cnt, " msgs: ", cnt*bulkCnt, " in ", duration, " rate: ", float64(cnt*bulkCnt)/duration.Seconds())
+	return nil
 
 }
 
@@ -212,7 +252,7 @@ func MakeRequests(cnt int, batchCnt int) []*pb.WriteRequest {
 		}
 		for bc := 0; bc < batchCnt; bc++ {
 			row := &pb.Row{
-				Data: []byte(fmt.Sprintf("{ \"id\": \"%s\", \"normalDate\": \"%s\",  \"woo\" : \"this thing\", \"is\": \"a bunch of extra data that we ddin't have in the thingy magiger bob before. But we do now don't we bob.\"}", uuid.NewV4(), time.Now().Format(time.RFC3339))),
+				Data: []byte(fmt.Sprintf("{ \"id\": \"%s\", \"normalDate\": \"%s\", \"commonEventId\":1021382,\"count\":1,\"direction\":2,\"domain\":\"SECIOUS\",\"entityId\":29,\"impactedEntityId\":29,\"impactedHostId\":175,\"insertedDate\":\"2016-10-07T15:54:46.1812332Z\",\"impactedIp\":\"10.128.64.155\",\"originIp\":\"10.110.0.63\",\"impactedLocationKey\":\"USCO:boulder\",\"logDate\":\"2016-10-07T08:57:19.5871955\",\"login\":\"chris.martin\",\"logSequence\":5973700,\"logSourceId\":924,\"mediatorSessionId\":134158,\"mpeRuleId\":1080954,\"msgClassId\":1060,\"msgSourceTypeId\":1000030,\"impactedName\":\"usbo1pprint01.schq.secious.com\",\"impactedNetworkId\":26,\"normalDate\":\"2016-10-07T14:57:19.7191955Z\",\"object\":\"\\\\\\\\*\\\\IPC$\",\"objectName\":\"spoolss\",\"originPort\":54562,\"priority\":22,\"rootEntityId\":1,\"session\":\"0x6a857adc\",\"severity\":\"Information\",\"vendorMessageId\":\"5145\",\"impactedZoneEnum\":1,\"originZoneEnum\":1,\"impactedGeoPoint\":{\"lat\":39.95960998535156,\"lon\":-105.51024627685547},\"resolvedOriginName\":\"94aeee81-8794-4c31-5ea1-b5b95aec1e6f\",\"resolvedImpactedName\":\"1fae203f-6745-430e-5a4a-61e44e30e288\",\"anonymizedLogin\":\"b7b986f64e2676ee41fc263c8b05a89ccce5a80e645a3a648048fd74e23d012b\",\"indexId\":\"24501ffb-824d-4c45-80c2-5f39ad70a783\",\"#companyName\":\"LogRhythm\"}", uuid.NewV4(), time.Now().Format(time.RFC3339))),
 			}
 			pr.Rows = append(pr.Rows, row)
 		}
